a586082c3ef5197eafaae4b986d806f0f7a7ac93
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2024-06-19 03:09:04 +0700

    Delete src directory

--- src/scripts/confirmEmailSuccess.js
+++
@@ -1,9 +0,0 @@
-import "../styles/style.css";
-import "../scripts/components/Header";
-import "../scripts/components/Footer";
-import { toggleMenu, closeMenu, navigateToChallenge, viewProfile } from "../scripts/function-nav";
-
-
-
-window.toggleMenu = toggleMenu;
-window.closeMenu = closeMenu;
