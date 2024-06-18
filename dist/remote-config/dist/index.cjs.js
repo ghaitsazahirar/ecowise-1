a552204d595c114cde473f6eed7e73d23d4a7116
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2024-06-19 03:41:11 +0700

    Add files via upload

---
+++ dist/remote-config/dist/index.cjs.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var remoteConfig=require("@firebase/remote-config");Object.keys(remoteConfig).forEach((function(e){"default"===e||exports.hasOwnProperty(e)||Object.defineProperty(exports,e,{enumerable:!0,get:function(){return remoteConfig[e]}})}));
