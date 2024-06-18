7bd919ac5e217ab3ff2a8f7ac0a31cb04e924a94
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2024-06-19 04:17:07 +0700

    Delete dist directory

--- dist/performance/dist/index.cjs.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var performance=require("@firebase/performance");Object.keys(performance).forEach((function(e){"default"===e||exports.hasOwnProperty(e)||Object.defineProperty(exports,e,{enumerable:!0,get:function(){return performance[e]}})}));
--- dist/remote-config/dist/index.cjs.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var remoteConfig=require("@firebase/remote-config");Object.keys(remoteConfig).forEach((function(e){"default"===e||exports.hasOwnProperty(e)||Object.defineProperty(exports,e,{enumerable:!0,get:function(){return remoteConfig[e]}})}));
